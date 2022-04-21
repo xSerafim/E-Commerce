db49d5245905f797d384ddad22c22b824cf25804
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-21 17:05:31 -0300

    feat(models/user): create user model

---
+++ back-end/models/user.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'Users',
+    }
+  );
+
+  return User;
+};
