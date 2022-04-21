398d4e020945052de303e4ae7d9b77e983540c63
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-21 17:05:02 -0300

    feat(models/sale): create sale model

---
+++ back-end/models/sale.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const Sale = sequelize.define(
+    'Sale',
+    {
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      totalPrice: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: 'created_date',
+      updatedAt: 'updated_date',
+      underscored: true,
+      tableName: 'Sales',
+    }
+  );
+
+  Sale.associate = (models) => {
+    Sale.hasMany(models.Item, {
+      foreignKey: 'item_id',
+      as: 'items',
+    });
+    Sale.haOne(models.User, {
+      foreignKey: 'user_id',
+      as: 'user',
+    });
+  };
+
+  return Sale;
+};
