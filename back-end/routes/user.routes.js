c21cd007684f66cd31c4d026c7e4f33f9b1ecef7
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-21 20:12:01 -0300

    feat(user.routes): add user login route

--- back-end/routes/user.routes.js
+++ back-end/routes/user.routes.js
@@ -1,11 +1,13 @@
 const userRouter = require('express').Router();
 const controller = require('../controllers/user');
 const { tokenValidation } = require('../middlewares/tokenValidation');
-const { userValidation } = require('../middlewares/userValidation');
+const joi = require('../middlewares/userValidation');
 
 userRouter
   .route('/')
-  .post(userValidation, controller.createUser)
+  .post(joi.createUserValidation, controller.createUser)
   .delete(tokenValidation, controller.deleteUser);
+
+userRouter.route('/login').get(controller.userLogin);
 
 module.exports = { userRouter };
