df5b2ed3f9e55b4805f2c323670feef7828c8c9c
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-21 22:13:51 -0300

    feat(categoryValidation): add joi validation to create category

---
+++ back-end/middlewares/categoryValidation.js
@@ -0,0 +1,15 @@
+const JOI = require('joi');
+
+const createSchema = JOI.object({
+  categoryName: JOI.string().required(),
+  subCategory: JOI.string().required(),
+});
+
+function createValidation(req, _res, next) {
+  const { error } = createSchema.validate(req.body);
+  if (error) throw error;
+
+  next();
+}
+
+module.exports = { createValidation };
