b4d50842bc58adce04932292bc94cab879ebeba5
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-21 22:21:31 -0300

    fix(serverErrorHandler): log err.message

--- back-end/utils/serverErrorHandler.js
+++ back-end/utils/serverErrorHandler.js
@@ -1,7 +1,7 @@
 const status = require('./requestStatus');
 
 function errorHandler(err) {
-  console.log(err);
+  console.log(err.message);
   return { code: status.SERVER_ERROR, message: err.message };
 }
 
