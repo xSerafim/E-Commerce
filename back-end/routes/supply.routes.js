4c78ce15e44ed18ec9b3f7202f547957e85f772b
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-23 09:12:49 -0300

    feat(supply.routes): add route to findAll and findById

--- back-end/routes/supply.routes.js
+++ back-end/routes/supply.routes.js
@@ -1,8 +1,13 @@
 const supplyRouter = require('express').Router();
 const supplyController = require('../controllers/supply');
 const auth = require('../middlewares/tokenValidation');
-// const joi = require('../middlewares/userValidation');
+const joi = require('../middlewares/supplyValidation');
 
-supplyRouter.route('/:id').put(auth.tokenValidation, supplyController.update);
+supplyRouter
+  .route('/:id')
+  .put(auth.tokenValidation, joi.updateValidation, supplyController.update)
+  .get(auth.tokenValidation, supplyController.findById);
+
+supplyRouter.route('/').get(auth.tokenValidation, supplyController.findAll);
 
 module.exports = { supplyRouter };
