f52f50ef2d9afd7d366f395796ad6d817b134114
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-23 18:02:32 -0300

    feat(sale.routes): joi validate post route

--- back-end/routes/sale.routes.js
+++ back-end/routes/sale.routes.js
@@ -1,11 +1,11 @@
 const saleRouter = require('express').Router();
 const saleController = require('../controllers/sale');
 const auth = require('../middlewares/tokenValidation');
-// const joi = require('../middlewares/adValidation');
+const joi = require('../middlewares/saleValidation');
 
 saleRouter
   .route('/')
-  .post(auth.tokenValidation, saleController.create)
+  .post(auth.tokenValidation, joi.createValidation, saleController.create)
   .get(auth.tokenValidation, saleController.findAll);
 
 saleRouter.route('/:id').get(auth.tokenValidation, saleController.findById);
