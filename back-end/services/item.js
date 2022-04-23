cd98a8812bf5c302340f5af0e0b1e1c9d5799610
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-22 21:47:06 -0300

    feat(services/item): create item services - add func to findAll items - add func to findById items - add func to create items - add func to delete items

---
+++ back-end/services/item.js
@@ -0,0 +1,64 @@
+const { Item } = require('../models');
+const categoryServices = require('./category');
+const server = require('../utils/serverErrorHandler');
+const status = require('../utils/requestStatus');
+const serialize = require('../utils/serializer');
+
+async function findAll() {
+  try {
+    const items = await Item.findAll();
+    return { code: status.OK, message: items };
+  } catch (err) {
+    return server.errorHandler(err);
+  }
+}
+
+async function findById(id) {
+  try {
+    const item = await Item.findOne({ where: { id } });
+
+    if (item) return { code: status.OK, message: item };
+
+    return { code: status.NOT_FOUND, message: 'Item not found' };
+  } catch (err) {
+    return server.errorHandler(err);
+  }
+}
+
+// ---Admin funcs--- //
+async function create(data) {
+  try {
+    const category = await categoryServices.findById(data.categoryId);
+    if (category.code === status.BAD_REQUEST) return category;
+
+    const serializedData = serialize.item(data);
+
+    const itemExist = await Item.findOne({ where: serializedData });
+
+    if (itemExist)
+      return { code: status.BAD_REQUEST, message: 'Item already exist' };
+
+    await Item.create(serializedData);
+
+    return { code: status.CREATED, message: 'Created new item succesfully' };
+  } catch (err) {
+    return server.errorHandler(err);
+  }
+}
+
+async function destroy(id) {
+  try {
+    const itemExist = await Item.findOne({ where: { id } });
+
+    if (!itemExist)
+      return { code: status.BAD_REQUEST, message: 'Item doesn"t exist' };
+
+    await Item.destroy({ where: { id } });
+    return { code: status.OK, message: 'Deleted item succesfully' };
+  } catch (err) {
+    return server.errorHandler(err);
+  }
+}
+// ----------------- //
+
+module.exports = { findAll, findById, create, destroy };
