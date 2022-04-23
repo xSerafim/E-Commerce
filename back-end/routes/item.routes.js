b038a0bd7011e32672610147b8bc66c3a5a899fd
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-22 21:41:43 -0300

    feat(item.routes): create item router - add get all - add post - add get by id - add delete by id

---
+++ back-end/routes/item.routes.js
@@ -0,0 +1,16 @@
+const itemRouter = require('express').Router();
+const itemController = require('../controllers/item');
+const auth = require('../middlewares/tokenValidation');
+const joi = require('../middlewares/itemValidation');
+
+itemRouter
+  .route('/')
+  .get(auth.tokenValidation, itemController.findAll)
+  .post(auth.tokenValidation, joi.createValidation, itemController.create);
+
+itemRouter
+  .route('/:id')
+  .get(auth.tokenValidation, itemController.findById)
+  .delete(auth.tokenValidation, itemController.destroy);
+
+module.exports = { itemRouter };
