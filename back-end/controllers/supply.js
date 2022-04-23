62c477cc166fe9e1df8dfeea63967e68fcc110c4
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-23 09:13:19 -0300

    feat(controllers): add request to findAll and byId

--- back-end/controllers/supply.js
+++ back-end/controllers/supply.js
@@ -8,4 +8,17 @@ async function update(req, res) {
   return res.status(code).json({ message });
 }
 
-module.exports = { update };
+async function findAll(_req, res) {
+  const { code, message } = await supplyServices.findAll();
+
+  return res.status(code).json({ message });
+}
+
+async function findById(req, res) {
+  const { id } = req.params;
+  const { code, message } = await supplyServices.findById(id);
+
+  return res.status(code).json({ message });
+}
+
+module.exports = { update, findAll, findById };
