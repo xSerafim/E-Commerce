bb9f40aac44dfe0b5d021c8de04474efbdd44870
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-24 19:51:12 -0300

    feat(Pages/Login): create login page

---
+++ front-end/src/Pages/Login/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from '../../Components/SignIn';
+import SignUp from '../../Components/SignUp';
+
+export default function Login() {
+  return (
+    <div>
+      <SignIn />
+      <SignUp />
+    </div>
+  );
+}
