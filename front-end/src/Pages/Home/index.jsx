149ad700cf9edc90b81f3dcebf53201c95df5716
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-25 17:42:20 -0300

    feat(Pages/Home): import and render header component

--- front-end/src/Pages/Home/index.jsx
+++ front-end/src/Pages/Home/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../../Components/Header';
 
 export default function Home() {
   return (
     <div>
-      <h1>Bem Vindo a Loja</h1>
+      <Header />
     </div>
   );
 }
