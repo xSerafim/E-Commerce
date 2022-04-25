884ce4ab669eadcdd3cf124bf4d406cab3a9c4b2
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-25 06:41:36 -0300

    commit teste 2

--- front-end/src/Components/SignIn/index.jsx
+++ front-end/src/Components/SignIn/index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-import { signInSchema } from "../../Utils/Validations";
-import { handleFetch } from "../../Services/Api";
-import { url } from "../../Utils/Endpoints";
-import { method } from "../../Utils/Methods";
+import { signInSchema } from '../../Utils/Validations';
+import { handleFetch } from '../../Services/Api';
+import { url } from '../../Utils/Endpoints';
+import { method } from '../../Utils/Methods';
 
 export default function SignIn() {
   const {
@@ -18,16 +18,16 @@ export default function SignIn() {
 
   const onSubmit = async (data) => {
     const response = await handleFetch(method.GET, url.userLogin, data);
-    console.log(response, "commit");
+    console.log(response, 'commit');
   };
 
   return (
     <div>
       <h1>Já sou cliente</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="email" placeholder="E-mail" {...register("email")} />
+        <input type="email" placeholder="E-mail" {...register('email')} />
         <p>{errors?.email?.message}</p>
-        <input type="password" placeholder="Senha" {...register("password")} />
+        <input type="password" placeholder="Senha" {...register('password')} />
         <p>{errors?.password?.message}</p>
         <input type="submit" />
       </form>
