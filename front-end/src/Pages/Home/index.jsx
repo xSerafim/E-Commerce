6e2b25b3414504c9f743a99826eaadea60ef3ddc
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-25 22:23:20 -0300

    feat(front-end): implement logic to render productCard in home page

--- front-end/src/Pages/Home/index.jsx
+++ front-end/src/Pages/Home/index.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../Components/Header';
+import { handleFetch } from '../../Services/Api';
+import { method } from '../../Utils/Methods';
+import { url } from '../../Utils/Endpoints';
+import ProductCard from '../../Components/Cards/ProductCard';
+import './style.css';
 
 export default function Home() {
+  const [ads, setAds] = useState('');
+
+  useEffect(() => {
+    handleFetch(method.GET, url.ads).then(({ data: { message } }) =>
+      setAds(message)
+    );
+  }, []);
+
   return (
-    <div>
+    <div className="home-container">
       <Header />
+      <div className="home-products">
+        {ads ? (
+          ads.map(({ items, adId }) => (
+            <ProductCard
+              key={adId}
+              name={items[0].name}
+              image={items[0].image}
+              price={items[0].price}
+            />
+          ))
+        ) : (
+          <span>Carregando...</span>
+        )}
+      </div>
     </div>
   );
 }
