0634632e012f328e0ce72a3117324a2194126f69
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-27 10:16:30 -0300

    feat(front-end): stylize header component

--- front-end/src/Components/Header/index.jsx
+++ front-end/src/Components/Header/index.jsx
@@ -4,33 +4,56 @@ import './style.css';
 
 export default function Header() {
   return (
-    <div className="full-header">
-      <div className="primary-header">
+    <header className="full-header">
+      <nav className="primary-header">
         <div className="header-logo">
-          <Link to="/">Loja de Roupas</Link>
+          <Link className="link" to="/">
+            Concept
+          </Link>
         </div>
-        <div className="header-search" />
-        <div className="login-cart">
-          <div className="login">
-            <Link to="/login">Login</Link>
-          </div>
-          <div className="cart">
-            <Link to="/login">Carrinho</Link>
-          </div>
+        <div className="mobile-menu">
+          <i className="fa-solid fa-bars" />
         </div>
-      </div>
-      <div className="header-menu">
-        <nav className="navigation">
-          <span>Masculino</span>
-          <span>Feminino</span>
-        </nav>
-        <form>
-          <input placeholder="O que você procura?" type="text" />
-          <button type="submit">
+        <form className="search-desktop">
+          <input
+            className="search-input"
+            placeholder="O que você procura?"
+            type="text"
+          />
+          <button className="search-button" type="submit">
+            <i className="fa-solid fa-magnifying-glass search-logo" />
+          </button>
+        </form>
+        <ul className="nav-list">
+          <li className="header-home">
+            <Link className="link" to="/">
+              <i className="fa-solid fa-house" />
+            </Link>
+          </li>
+          <li className="login">
+            <Link className="link" to="/login">
+              <i className="fa-solid fa-user" />
+            </Link>
+          </li>
+          <li className="cart">
+            <Link className="link" to="/login">
+              <i className="fa-solid fa-cart-shopping" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="search-box-mobile">
+        <form className="search-mobile">
+          <input
+            className="search-input"
+            placeholder="O que você procura?"
+            type="text"
+          />
+          <button className="search-button" type="submit">
             <i className="fa-solid fa-magnifying-glass search-logo" />
           </button>
         </form>
       </div>
-    </div>
+    </header>
   );
 }
