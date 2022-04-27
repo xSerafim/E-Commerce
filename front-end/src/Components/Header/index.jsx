2097e51efe6f31355bb2d9f8d26d0f392823052a
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-27 20:18:34 -0300

    feat(front-end): stylize header component

--- front-end/src/Components/Header/index.jsx
+++ front-end/src/Components/Header/index.jsx
@@ -3,16 +3,39 @@ import { Link } from 'react-router-dom';
 import './style.css';
 
 export default function Header() {
+  const navList = document.getElementsByClassName('nav-list');
+  const mobileLinks = document.getElementsByClassName('mobile-link');
+
+  const animateLinks = () => {
+    for (let i = 0; i < mobileLinks.length; i += 1) {
+      if (mobileLinks[i].style.animation) {
+        mobileLinks[i].style.animation = '';
+      } else {
+        mobileLinks[i].style.animation = `navLinkFade 0.5s ease forwards ${
+          i / 7 + 0.3
+        }s`;
+      }
+    }
+    console.log(mobileLinks[0].style.animation);
+  };
+
   return (
     <header className="full-header">
       <nav className="primary-header">
+        <button
+          onClick={() => {
+            navList[0].classList.toggle('active');
+            animateLinks();
+          }}
+          type="button"
+          className="mobile-menu"
+        >
+          <i className="fa-solid fa-bars" />
+        </button>
         <div className="header-logo">
           <Link className="link" to="/">
             Concept
           </Link>
-        </div>
-        <div className="mobile-menu">
-          <i className="fa-solid fa-bars" />
         </div>
         <form className="search-desktop">
           <input
@@ -25,17 +48,17 @@ export default function Header() {
           </button>
         </form>
         <ul className="nav-list">
-          <li className="header-home">
+          <li className="header-home mobile-link">
             <Link className="link" to="/">
               <i className="fa-solid fa-house" />
             </Link>
           </li>
-          <li className="login">
+          <li className="login mobile-link">
             <Link className="link" to="/login">
               <i className="fa-solid fa-user" />
             </Link>
           </li>
-          <li className="cart">
+          <li className="cart mobile-link">
             <Link className="link" to="/login">
               <i className="fa-solid fa-cart-shopping" />
             </Link>
