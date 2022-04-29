27c1e562ed8dec4d68ef873d8186e00f4cd83e1a
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-28 21:26:20 -0300

    feat(front-end): add new routes

--- front-end/src/Routes/index.jsx
+++ front-end/src/Routes/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Cart from '../Pages/Cart';
+import Favorites from '../Pages/Favorites';
 import Home from '../Pages/Home';
 import Login from '../Pages/Login';
 import ProductAd from '../Pages/ProductAd';
@@ -10,6 +12,8 @@ export default function RoutesComponent() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/product/:id" element={<ProductAd />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/favorites" element={<Favorites />} />
     </Routes>
   );
 }
