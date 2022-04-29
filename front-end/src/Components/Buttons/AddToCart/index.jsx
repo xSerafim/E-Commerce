f3dc69638d01de11cf366861dece39add5f004ab
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-28 21:20:55 -0300

    feat(front-end): create button AddToCart

---
+++ front-end/src/Components/Buttons/AddToCart/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AddToCartBtn() {
+  return (
+    <div className="add-to-cart">
+      <button type="button">Adicionar ao Carrinho</button>
+    </div>
+  );
+}
