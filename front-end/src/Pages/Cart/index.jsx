98c97031ac080c419b15e4508a08d70eefbb4d3e
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-28 21:25:35 -0300

    feat(front-end): create Cart Page

---
+++ front-end/src/Pages/Cart/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../Components/Header';
+
+export default function Cart() {
+  return (
+    <div>
+      <Header />
+      <h1>CARRINHO</h1>
+    </div>
+  );
+}
