176be3ce5f4a2c275d2e0251d65a3563cdd7f18d
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-30 14:42:03 -0300

    feat(front-end): render CartCards in CartPage

--- front-end/src/Pages/Cart/index.jsx
+++ front-end/src/Pages/Cart/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import CartCard from '../../Components/Cards/CartCard';
+import './style.css';
+
 import Header from '../../Components/Header';
 
 export default function Cart() {
+  const items = useSelector((state) => state.items);
+
   return (
     <div>
       <Header />
-      <h1>CARRINHO</h1>
+      <div className="cart-main">
+        {items && items.map((item) => <CartCard key={item.id} item={item} />)}
+      </div>
     </div>
   );
 }
