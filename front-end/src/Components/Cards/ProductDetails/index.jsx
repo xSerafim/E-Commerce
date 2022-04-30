df1839e4afe46a3f4deba7ed379b1a00e79ee162
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-29 22:25:46 -0300

    feat(front-end): add color and size validation in productDetails card

--- front-end/src/Components/Cards/ProductDetails/index.jsx
+++ front-end/src/Components/Cards/ProductDetails/index.jsx
@@ -18,6 +18,12 @@ export default function ProductDetails({ data }) {
 
   const colorsAndSizes = removeDuplicate(colors, items);
 
+  const [size, setSize] = useState('Selecione');
+  const [color, setColor] = useState(() => {
+    if (colors.length === 1) return colors[0];
+    return '';
+  });
+
   return (
     <div className="details-box">
       <div className="image-box">
@@ -31,28 +37,33 @@ export default function ProductDetails({ data }) {
       <div className="color-container">
         <p className="product-name">{image.name}</p>
         <p className="price detail-price">{`R$${image.price}`}</p>
-        <select>
+        <select onChange={({ target: { value } }) => setSize(value)}>
           <option>Selecione o tamanho...</option>
-          {colorsAndSizes[itemIndex].sizes.map((size) => (
-            <option key={size}>{size}</option>
+          {colorsAndSizes[itemIndex].sizes.map((currSize) => (
+            <option key={currSize}>{currSize}</option>
           ))}
         </select>
         <div>
           <p>Cores Disponíveis</p>
           <div>
-            {colorsAndSizes.map(({ color }, i) => (
+            {colorsAndSizes.map(({ color: currColor }, i) => (
               <button
                 className="color-button"
                 type="button"
-                key={color}
-                onClick={() => setItemIndex(i) > imageColor(i)}
+                key={currColor}
+                onClick={() =>
+                  setItemIndex(i) > imageColor(i) > setColor(currColor)
+                }
               >
-                <div className="color-box" style={{ backgroundColor: color }} />
+                <div
+                  className="color-box"
+                  style={{ backgroundColor: currColor }}
+                />
               </button>
             ))}
           </div>
         </div>
-        <AddToCartBtn />
+        <AddToCartBtn color={color} size={size} items={items} />
       </div>
     </div>
   );
