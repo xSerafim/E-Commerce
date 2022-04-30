2907e4889fe1c39eac6245bd46bbc334991ca40f
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-29 22:26:57 -0300

    feat(front-end): add logic to find selected item

--- front-end/src/Components/Buttons/AddToCart/index.jsx
+++ front-end/src/Components/Buttons/AddToCart/index.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import useLocalStorage from '../../../Hooks';
 
-export default function AddToCartBtn() {
+export default function AddToCartBtn({ color, size, items }) {
+  const [err, setErr] = useState('');
+  const [setValue] = useLocalStorage();
+
+  useEffect(() => {
+    setValue();
+  }, []);
+
+  const findItem = () =>
+    items
+      .filter((item) => item.color === color)
+      .filter((item) => item.size === size);
+
+  const handleClick = () => {
+    if (size.includes('Selecione')) return setErr('Selecione um tamanho');
+    if (!color) return setErr('Selecione uma cor');
+    setValue(findItem());
+    return setErr('');
+  };
+
   return (
     <div className="add-to-cart">
-      <button type="button">Adicionar ao Carrinho</button>
+      <button onClick={handleClick} type="button">
+        Adicionar ao Carrinho
+      </button>
+      {err && <p>{err}</p>}
     </div>
   );
 }
+
+AddToCartBtn.propTypes = {
+  color: PropTypes.string,
+}.required;
