4323f24c4ba6a35b15e4e95949748a04b8067fc3
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-30 14:41:28 -0300

    feat(front-end): add logic to remove item in useLocalStorage

--- front-end/src/Hooks/index.js
+++ front-end/src/Hooks/index.js
@@ -1,6 +1,8 @@
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { saveItem } from '../Store/Actions/saveItem';
+import subQuantityOfItems from '../Utils/Functions/subQuantityOfItem';
+import sumQuantityOfItems from '../Utils/Functions/sumQuantityOfItems';
 
 export default function useLocalStorage(key = 'cartItems', initialState = []) {
   const dispatch = useDispatch();
@@ -16,10 +18,17 @@ export default function useLocalStorage(key = 'cartItems', initialState = []) {
   };
 
   const setValue = useCallback(
-    (value = []) => {
+    (value = [], isSum = true) => {
       try {
-        dispatch(saveItem([...value, ...newItem()]));
-        localStorage.setItem(key, JSON.stringify([...value, ...newItem()]));
+        if (isSum) {
+          const itemsWithQuantity = sumQuantityOfItems(value, newItem());
+          dispatch(saveItem(itemsWithQuantity));
+          localStorage.setItem(key, JSON.stringify(itemsWithQuantity));
+        } else {
+          const itemsWithQuantity = subQuantityOfItems(value, newItem());
+          dispatch(saveItem(itemsWithQuantity));
+          localStorage.setItem(key, JSON.stringify(itemsWithQuantity));
+        }
       } catch (error) {
         console.log(error);
       }
