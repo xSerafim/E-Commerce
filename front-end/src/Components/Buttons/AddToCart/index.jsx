639011f0d3f9d1aff96dfacdd607f587bbff384a
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-05-01 14:13:59 -0300

    feat(front-end): add message when add to cart

--- front-end/src/Components/Buttons/AddToCart/index.jsx
+++ front-end/src/Components/Buttons/AddToCart/index.jsx
@@ -4,6 +4,7 @@ import useLocalStorage from '../../../Hooks';
 
 export default function AddToCartBtn({ color, size, items, adId }) {
   const [err, setErr] = useState('');
+  const [click, setClick] = useState(false);
   const [setValue] = useLocalStorage();
 
   const findItem = () =>
@@ -14,18 +15,27 @@ export default function AddToCartBtn({ color, size, items, adId }) {
   const handleClick = () => {
     if (size.includes('Selecione')) return setErr('Selecione um tamanho');
     if (!color) return setErr('Selecione uma cor');
+
     const itemWithAdId = findItem();
     itemWithAdId[0].adId = adId;
     setValue(itemWithAdId);
+
+    setClick(true);
+
+    setTimeout(() => {
+      setClick(false);
+    }, 2000);
+
     return setErr('');
   };
 
   return (
     <div className="add-to-cart">
+      {click && <p id="added-to-cart">Produto adicionado ao carrinho</p>}
+      {err && <p id="error-message">{err}</p>}
       <button onClick={handleClick} type="button">
         Adicionar ao Carrinho
       </button>
-      {err && <p>{err}</p>}
     </div>
   );
 }
