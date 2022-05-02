e0e5615181f0bf8a5f7e8ae6d34a2403d4c27a22
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-05-02 18:28:42 -0300

    feat(front-end): import and stylize component ConfirmSaleBn in page CART

--- front-end/src/Pages/Cart/index.jsx
+++ front-end/src/Pages/Cart/index.jsx
@@ -5,6 +5,7 @@ import './style.css';
 
 import Header from '../../Components/Header';
 import FinishSale from '../../Components/FinishSale';
+import ConfirmSaleBtn from '../../Components/Buttons/ConfirmSale';
 
 export default function Cart() {
   const items = useSelector((state) => state.items);
@@ -15,6 +16,7 @@ export default function Cart() {
       <div className="cart-main">
         {items && items.map((item) => <CartCard key={item.id} item={item} />)}
         <FinishSale />
+        <ConfirmSaleBtn />
       </div>
     </div>
   );
